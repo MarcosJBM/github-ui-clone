ab97628dce32171931146e257f84af78604dfc9b
Marcos Botene <marcosjbotene@gmail.com>
2021-01-05 20:22:46 -0300

    Structure and CSS added to the Profile page.

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,8 +1,29 @@
+import ProfileData from "../../components/ProfileData";
+
+import { Container, Main, LeftSide, RightSide } from "./styles";
+
 const Profile = () => {
   return (
-    <div>
-      <h1>Profile</h1>
-    </div>
+    <Container>
+      <Main>
+        <LeftSide>
+          <ProfileData
+            username={"marcosjbm"}
+            name={"marcosbotene"}
+            avatarUrl={
+              "https://avatars1.githubusercontent.com/u/67931411?s=460&u=e3c24342e72d189eb67a83305d3ef34562544e70&v=4"
+            }
+            followers={6}
+            following={9}
+            company={"Something"}
+            location={"Santa Catarina, Brazil"}
+            email={"marcosjbotene@gmail.com"}
+            blog={"Something"}
+          />
+        </LeftSide>
+        <RightSide></RightSide>
+      </Main>
+    </Container>
   );
 };
 
