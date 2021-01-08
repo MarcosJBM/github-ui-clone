7a6a1d02886cfd500d458c3c12d90ed4b83301d2
Marcos Botene <marcosjbotene@gmail.com>
2021-01-08 18:18:26 -0300

    Added Structure to Repo Page.

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,8 +1,52 @@
+import { Link } from "react-router-dom";
+import {
+  Container,
+  Breadcrumb,
+  RepoIcon,
+  Stats,
+  StarIcon,
+  ForkIcon,
+  LinkButton,
+  GitHubIcon,
+} from "./styles";
+
 const Repo = () => {
   return (
-    <div>
-      <h1>Repo</h1>
-    </div>
+    <Container>
+      <Breadcrumb>
+        <RepoIcon />
+        <Link className={"username"} to={"/marcosjbm"}>
+          marcosjbm
+        </Link>
+
+        <span>/</span>
+
+        <Link className={"reponame"} to={"/marcosjbm/user-search"}>
+          user-search
+        </Link>
+      </Breadcrumb>
+
+      <p>A ReactJS application using the GitHub API.</p>
+
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>9</b>
+          <span>stars</span>
+        </li>
+
+        <li>
+          <ForkIcon />
+          <b>2</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+
+      <LinkButton href={"https://github.com/MarcosJBM/User-Search"}>
+        <GitHubIcon />
+        <span>View on Github</span>
+      </LinkButton>
+    </Container>
   );
 };
 
