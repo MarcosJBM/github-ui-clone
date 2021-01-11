57b9d6409b730906e5a19c702c82308756010f2e
Marcos Botene <marcosjbotene@gmail.com>
2021-01-11 10:17:21 -0300

    Search added.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { Container, Githublogo, SearchForm } from "./styles";
 
 const Header = () => {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+
+    navigate("/" + search.toLocaleLowerCase().trim());
+  }
+
   return (
     <Container>
       <Githublogo />
-      <SearchForm>
-        <input type='text' />
+      <SearchForm onSubmit={handleSubmit}>
+        <input
+          type='text'
+          value={search}
+          onChange={e => setSearch(e.currentTarget.value)}
+        />
       </SearchForm>
     </Container>
   );
