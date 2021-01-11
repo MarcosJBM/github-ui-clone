7e7fddd03418ca30faf31cb10294995772da02ae
Marcos Botene <marcosjbotene@gmail.com>
2021-01-11 16:38:03 -0300

    Dark mode added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,36 @@
 import "react-calendar-heatmap/dist/styles.css";
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Footer from "./components/Footer";
-
-import Header from "./components/Header";
+import { ThemeProvider } from "styled-components";
 
 import Profile from "./pages/Profile";
 import Repo from "./pages/Repo";
 
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 import GlobalStyles from "./styles/GlobalStyles";
+import { ThemeName, themes } from "./styles/themes";
 
 const App = () => {
+  const [themeName, setThemeName] = useState<ThemeName>("light");
+  const currentTheme = themes[themeName];
+
   return (
-    <BrowserRouter>
-      <Header />
+    <ThemeProvider theme={currentTheme}>
+      <BrowserRouter>
+        <Header themeName={themeName} setThemeName={setThemeName} />
 
-      <Routes>
-        <Route path='/' element={<Profile />} />
-        <Route path='/:username' element={<Profile />} />
-        <Route path='/:username/:repo' element={<Repo />} />
-      </Routes>
+        <Routes>
+          <Route path='/' element={<Profile />} />
+          <Route path='/:username' element={<Profile />} />
+          <Route path='/:username/:repo' element={<Repo />} />
+        </Routes>
 
-      <Footer />
-      <GlobalStyles />
-    </BrowserRouter>
+        <Footer />
+        <GlobalStyles />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
 
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,14 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { ThemeName } from "../../styles/themes";
 import { Container, Githublogo, SearchForm } from "./styles";
 
-const Header = () => {
+interface Props {
+  themeName: ThemeName;
+  setThemeName: (newTheme: ThemeName) => void;
+}
+
+const Header = ({ themeName, setThemeName }: Props) => {
   const [search, setSearch] = useState("");
   const navigate = useNavigate();
 
@@ -12,9 +18,13 @@ const Header = () => {
     navigate("/" + search.toLocaleLowerCase().trim());
   }
 
+  function toggleTheme() {
+    setThemeName(themeName === "light" ? "dark" : "light");
+  }
+
   return (
     <Container>
-      <Githublogo />
+      <Githublogo onClick={toggleTheme} />
       <SearchForm onSubmit={handleSubmit}>
         <input
           type='text'
